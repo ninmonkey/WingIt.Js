297c682892c8b986ca1ec4563d8d0e9e02215d44
Jake Bolton <ninmonkeys@gmail.com>
2025-09-09 14:06:03 -0500

    add: `labelPoint` for `drawing.js` and responsive canvas sizing

--- js/boid.js
+++ js/boid.js
@@ -1,3 +1,4 @@
+import { labelPoint } from './drawing.js';
 export class Vector2 {
     /**
      * @description Represents a 2D vector
@@ -5,7 +6,7 @@ export class Vector2 {
      */
     #x = 0
     #y = 0
-    constructor ( x = 0, y = 0 ) {
+    constructor( x = 0, y = 0 ) {
         this.Position( x, y )
     }
     set x ( value ) { this.#x = value; }
@@ -19,24 +20,24 @@ export class Vector2 {
         this.#x = x;
         this.#y = y;
     }
-    toString() {
+    toString () {
         return `Vector2(${ this.#x }, ${ this.#y })`;
     }
 }
- export function WrapScreenEdge( boid ) {
+export function WrapScreenEdge ( boid ) {
     /**
      * @description Wraps boid around edges like pac-man or asteroids
      */
-    if( boid.Position.x > canvas.width ) {
+    if ( boid.Position.x > canvas.width ) {
         boid.Position.x = 0
     }
-    if( boid.Position.x < 0 ) {
+    if ( boid.Position.x < 0 ) {
         boid.Position.x = canvas.width
     }
-    if( boid.Position.y > canvas.height ) {
+    if ( boid.Position.y > canvas.height ) {
         boid.Position.y = 0
     }
-    if( boid.Position.y < 0 ) {
+    if ( boid.Position.y < 0 ) {
         boid.Position.y = canvas.height
     }
 }
@@ -50,7 +51,8 @@ export class Boid {
     #velocity = new Vector2( 10.0, 10.0 );
     // #acceleration = new Vector2( 0.0, 0.0 );
 
-    #colorFill = 'rgb(27, 151, 182)'
+    #fillStyle = 'rgb(27, 151, 182)'
+    #radius = 20
 
     // constructor (
     //     position = new Vector2( 0.0, 0.0 ),
@@ -61,18 +63,78 @@ export class Boid {
     get Velocity () { return this.#velocity; }
     // get Acceleration () { return this.#acceleration; }
 
-    draw( context ) {
-        if( context == null ) { throw   new Error('Boid.draw() requires a valid 2D rendering context as an argument'); }
-        context.fillStyle = this.#colorFill;
-        // context.fillRect( this.Position.x,  this.Position.y,  this.Position.x + 4,  this.Position.y + 4);
-        context.fillRect( this.Position.x,  this.Position.y,
-            this.Position.x + 10, this.Position.y + 10);
+    draw ( context ) {
+        /**
+         * @description basic rendering of boid. future will be extracted
+         * @param context {CanvasRenderingContext2D} 2D rendering context from HTML5 canvas
+         */
+        const ctx = context
+        if ( ctx == null ) { throw new Error( 'Boid.draw() requires a valid 2D rendering context as an argument' ); }
+        ctx.fillStyle = this.FillStyle;
+
+        const gradient = ctx.createConicGradient( 0, 100, 100 );
+        const grad_0 = 'hsl(200 100% 50% / .25 )'; // hsl(200 100% 50% )
+        const grad_1 = 'hsl(263 100% 50% / .70)'; // hsl(200 100% 50% )
+        gradient.addColorStop( 0, grad_0 );
+        gradient.addColorStop( 0.50, grad_1 );
+        gradient.addColorStop( 1, grad_0 );
+
+        // ctx.fillStyle = 'goldenrod'
+        // ctx.strokeStyle = 'red'
+        ctx.fillStyle = gradient
+        ctx.fillStyle = 'salmon'
+        ctx.fillStyle = 'hsl(200 100% 50% / 1.0'
+        // ctx.fillStyle = 'red'
+        // ctx.strokeStyle = 'red'
+        ctx.strokeStyle = gradient
+        ctx.lineWidth = this.Radius * .5
+        ctx.beginPath();
+        // ctx.arc( this.Position.x, this.Position.y, 5, 0, Math.PI * 2, true );
+        /* arc(x, y, radius, startAngle, endAngle, counterclockwise) */
+        ctx.arc(this.Position.x, this.Position.y, this.Radius, 0, Math.PI * 2, true);
+        ctx.stroke()
+        ctx.fill()
+        ctx.beginPath();
+        ctx.arc( 100, 200, 50, 10, 40, false );
+        ctx.stroke()
+        ctx.fill()
+
+        // ctx.fillRect( this.Position.x,  this.Position.y,  this.Position.x + 4,  this.Position.y + 4);
+
+        ctx.beginPath()
+        ctx.lineWidth = 20
+        ctx.strokeStyle = 'salmon'
+        ctx.arc( 100, 75, 50, 0, 2 * Math.PI );
+        ctx.fillStyle = gradient
+        ctx.fillStyle = 'hsl(200 100% 50% / .75)'
+        ctx.strokeStyle = gradient
+
+        ctx.stroke()
+        ctx.fill()
+
+        labelPoint( ctx, this.Position, {
+            // font: 'bold 2rem serif',
+            // font: 'bold 5rem "fira sans", mono',
+            font: `bold 1rem "Cascadia Code", mono`,
+            fillStyle: 'rgb( 10% 10% 10% / .8)',
+        } );
+
+        labelPoint( ctx, { x: 100, y: 300 }, {
+            font: 'bold 2rem serif'
+        } );
+        labelPoint( ctx, { x: 200, y: 200 } );
+
+
+        // ctx.fillRect( this.Position.x,  this.Position.y,
+        // this.Position.x + 10, this.Position.y + 10);
     }
 
-    toString() {
+    toString () {
         return `Boid( Position: ${ this.#position.toString() }, Velocity: ${ this.#velocity.toString() } )`;
     }
-    set FillStyle( colorString ) {
-        this.#colorFill = colorString;
+    get Radius() { return this.#radius; }
+    get FillStyle () { return this.#fillStyle; }
+    set FillStyle ( colorString ) {
+        this.#fillStyle = colorString;
     }
 }
---
+++ js/drawing.js
@@ -0,0 +1,28 @@
+export function labelPoint ( ctx, p, options  = {} ) {
+    /**
+     * @description Labels coordinates on the canvas
+     * @param ctx {CanvasRenderingContext2D} 2D rendering context from HTML5 canvas
+     * @param p {x,y} Coordinates
+     * @param options {Object} Options for labeling
+     * @param options.fillStyle {string} style for `fillText`
+     * @param options.font {string|null} CSS font string, e.g. '48px serif'
+     * @param options.offset {number} pixel offset for text label
+     * @param options.toFixed {number} number of decimal places to display
+     */
+    const defaults = {
+        // fillStyle: 'rgb( 10% 10% 10% / 1.0)',
+        fillStyle: 'rgb( 10% 10% 10% / .8)',
+        // font     : '1.5rem mono',
+        font: `bold 1rem "Cascadia Code", mono`,
+        offset   : 10,
+        toFixed  : 0,
+    };
+    const config = { ...defaults, ...options,  };
+
+    const x = p.x.toFixed( config.toFixed  );
+    const y = p.y.toFixed( config.toFixed );
+
+    ctx.font      = config.font
+    ctx.fillStyle = config.fillStyle
+    ctx.fillText( `(${ x }, ${ y })`, p.x + config.offset, p.y + config.offset );
+}
